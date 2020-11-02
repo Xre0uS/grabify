9072718f7192fbebd780107eb11487c79a09ae0a
mashmllo <52559284+mashmllo@users.noreply.github.com>
2020-11-02 17:00:26 +0800

    Update editReview.php

--- php/editReview.php
+++ php/editReview.php
@@ -17,8 +17,8 @@
     <link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'>
     <link rel='stylesheet' href='https://raw.githubusercontent.com/kartik-v/bootstrap-star-rating/master/css/star-rating.min.css'>
 
-   <script src="starThingy/jquery.star-rating-svg.js"></script>
-    <link rel="stylesheet" type="text/css" href="starThingy/star-rating-svg.css">
+   <script src="js/jquery.star-rating-svg.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/star-rating-svg.css">
      
 
 </head>
