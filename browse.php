f373d2f66bd37b94fc50d71bf82dd9546f5a3103
Haowen <36565320+AreiousX@users.noreply.github.com>
2020-11-02 15:30:02 +0800

    Add files via upload

--- browse.php
+++ browse.php
@@ -1,1 +1,22 @@
+<!DOCTYPE html>
 
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        <?php include 'css/styles.css'; ?>
+    </style>
+    <title>Grabify - Browse</title>
+</head>
+
+<body>
+
+    <?php include 'php/navbar.php'; ?>
+
+</body>
+
+</html>
