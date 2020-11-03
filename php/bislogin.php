f331d7fcb5f95099a2feee36bdc61f281159237b
Jeric-png <69560421+Jeric-png@users.noreply.github.com>
2020-11-03 14:49:06 +0800

    Add files via upload

---
+++ php/bislogin.php
@@ -0,0 +1,45 @@
+<head>
+  
+<style>
+<?php include 'css/bispage.css'; ?>
+<?php include 'css/bistable.css'; ?>
+<?php include 'css/style.css'; ?>
+</style>
+
+</head>
+<body>
+
+<h2>Business Login to Grabify </h2>
+
+<br>
+<div class="container">
+  <form action="bis.php">
+  <div class="row">
+    <div class="col-25">
+      <label style="fontsize:50px;" for="username">User Name</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="username" name="username" placeholder="User Name...">
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-25">
+      <label for="password">Password:</label>
+    </div>
+    <div class="col-75">
+      <input type="text" id="password" name="password" placeholder="Password...">
+    </div>
+  </div>
+  <div class="row">
+    <input type="submit" value="Login">
+  </div>
+  </div>
+  </div>
+  </form>
+</body>
+
+<footer>
+  CopyRight Temasek Polytechnic
+</footer>
+
+
