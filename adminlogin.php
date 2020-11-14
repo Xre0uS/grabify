091065146326527881ea111cf02ccde25f856ee6
Haowen <36565320+AreiousX@users.noreply.github.com>
2020-11-14 18:35:06 +0800

    Changes login pages to test login functions

--- adminlogin.php
+++ adminlogin.php
@@ -1,25 +1,18 @@
-<head>
-<meta charset="UTF-8">
-<title>Log In</title>
+<!DOCTYPE html>
 
-<link rel="stylesheet" href="css/style.css" />
+<head>
+  <meta charset="UTF-8">
+  <title>Log In</title>
+
+  <link rel="stylesheet" href="css/style.css" />
 
 </head>
 
 <style>
-<?php include 'css/adminlogin.css'; ?>
+  <?php include 'css/styles.css'; ?>
 </style>
+
 <body>
-
-<form>
-
-	<h2>Admin Login</h2>
-
-	<input type="text" name="username" class="text-field" placeholder="Username" />
-    <input type="password" name="password" class="text-field" placeholder="Password" />
-    
-  <a href="manadmin.php"><input type="button"  class="button" value="Log In" /></a>
-
-</form>
+  <?php include 'php/navbar.php'; ?>
 
 </body>
