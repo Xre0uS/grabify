b08de5d0efc4720323049ac6ee3f8995caae9826
Haowen <zhenghaowenn@gmail.com>
2020-12-04 12:16:39 +0800

    Updated navbar functionality

--- js/navbar.js
+++ js/navbar.js
@@ -39,12 +39,30 @@ function adminLogin() {
     $(document).ready(function () {
         $("#loginBtnContainer").load('php/adminloginfn.php');
     });
+    setLogoText("Grabify Admin");
 }
 
 function businessLogin() {
     $(document).ready(function () {
         $("#loginBtnContainer").load('php/bisloginfn.php');
     });
+    setLogoText("Grabify Business");
+}
+
+function setLogoText(text) {
+    document.getElementById("logoText").innerText = text;
+}
+
+function logoAction() {
+    if (window.location.href.includes("admin")) {
+        window.location.href = "adminlogin.php"
+    }
+    else if (window.location.href.includes("bis")) {
+        window.location.href = "bislogin.php"
+    }
+    else {
+        window.location.href = "home.php"
+    }
 }
 
 /*
