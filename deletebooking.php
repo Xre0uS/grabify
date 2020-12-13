8f8f99abe6df3ff3ea2e6fcbcbd1035ff22784c6
Clarence <tanweisheng@msn.com>
2020-12-13 20:01:56 +0800

    Add files via upload

---
+++ deletebooking.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+session_regenerate_id(); //session_regenerate_id();
+
+?>
+<?php
+
+
+include 'config.php'; //Getting the connection via config.php
+
+
+if(isset($_SESSION["username"]))
+{
+    if(isset($_GET['Submit']) && $_GET['Submit'] === "Delete"){
+        $bookingID=$_GET['booking_id'];
+        
+        
+        $query= $con->prepare("Delete from booking where booking_id = ?");
+        $query->bind_param('i', $bookingID); //bind the parameters
+        
+        if ($query->execute()){  //execute query
+            header( "refresh:0;url=booking.php" );
+}
+else
+{
+
+}
+
+    }
+
+}
+?>
