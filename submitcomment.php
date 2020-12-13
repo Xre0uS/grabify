392345a05a521b27db70d7bd48c8574b1227853a
ClarenceTohHaoZhong <69560999+ClarenceTohHaoZhong@users.noreply.github.com>
2020-12-13 22:49:27 +0800

    Update submitcomment.php

--- submitcomment.php
+++ submitcomment.php
@@ -31,6 +31,9 @@ $stmt=$con->prepare("SELECT user_id FROM users WHERE username = ?");//Get produc
     $res=$stmt->execute();
     $stmt->store_result();
     $stmt->bind_result($userID); //Bind the data from database
+while ($stmt->fetch()){
+            $userID = $userID
+    }
 	
 $ratingpattern = "/^[a-zA-Z0-9\s]*$/";
 	if (!preg_match($ratingpattern, $rating)){
