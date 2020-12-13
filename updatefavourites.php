4171fdc3668404d565dc515604ef923f50fd59d5
ClarenceTohHaoZhong <69560999+ClarenceTohHaoZhong@users.noreply.github.com>
2020-12-13 19:56:31 +0800

    Add files via upload

---
+++ updatefavourites.php
@@ -0,0 +1,16 @@
+<?php
+require('config.php');
+$favID = $_POST["favID"];
+$cat = $_POST["cat"];
+$stmt=$con->prepare("UPDATE favorite SET category=? WHERE fav_id=?"); //Update function
+$stmt->bind_param("ss", $cat, $favID);
+$res=$stmt->execute();
+if($res){
+    echo "UPDATE SUCCESSFUL";
+	header("location:favouritespage.php");
+	}
+else{
+    echo "UNABLE TO UPDATE";
+	header("location:favouritespage.php");
+    }
+?>
