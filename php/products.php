1415784198aea7bb79e5a7229b16fdf0cd3fe51c
ClarenceTohHaoZhong <69560999+ClarenceTohHaoZhong@users.noreply.github.com>
2020-12-13 17:30:39 +0800

    Update products.php

--- php/products.php
+++ php/products.php
@@ -1,19 +1,18 @@
-<?php include 'navbar.php'; ?>
+<?php include 'userloginfn.php'; ?>
 <?php
-session_start();
-session_regenerate_id(); //regenerate new session id
+
 require('config.php');
 // To check if session is started.
 if(isset($_SESSION["username"]))
 {
 	$username = $_SESSION["username"];
-$stmt=$con->prepare("SELECT user_id FROM users WHERE username = ?");//Get product data from database
+	$stmt=$con->prepare("SELECT user_id FROM users WHERE username = ?");//Get product data from database
 	$stmt->bind_param("s", $username);
     $res=$stmt->execute();
     $stmt->store_result();
     $stmt->bind_result($userID); //Bind the data from database
-$userID = $userID;
-$stmt=$con->prepare("SELECT product.product_id, product.name, product.price, product.description, product.location, business.company_name FROM (product LEFT JOIN business ON product.business_business_id = business.business_id) GROUP BY product.product_id");//Get product data from database
+	$userID = $userID;
+	$stmt=$con->prepare("SELECT product.product_id, product.name, product.price, product.description, product.location, business.company_name FROM (product LEFT JOIN business ON product.business_business_id = business.business_id) GROUP BY product.product_id");//Get product data from database
     $res=$stmt->execute();
     $stmt->store_result();
     $stmt->bind_result($productID, $name, $price, $description, $location, $busName); //Bind the data from database
@@ -36,7 +35,7 @@ $stmt=$con->prepare("SELECT product.product_id, product.name, product.price, pro
 else
 {
 	
-$stmt=$con->prepare("SELECT product.product_id, product.name, product.price, product.description, product.location, business.company_name FROM (product LEFT JOIN business ON product.business_business_id = business.business_id) GROUP BY product.product_id");//Get product data from database
+	$stmt=$con->prepare("SELECT product.product_id, product.name, product.price, product.description, product.location, business.company_name FROM (product LEFT JOIN business ON product.business_business_id = business.business_id) GROUP BY product.product_id");//Get product data from database
     $res=$stmt->execute();
     $stmt->store_result();
     $stmt->bind_result($productID, $name, $price, $description, $location, $busName); //Bind the data from database
