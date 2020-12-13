366d7bceaf436f8a6e1dc11dca09f277c6ece92f
ClarenceTohHaoZhong <69560999+ClarenceTohHaoZhong@users.noreply.github.com>
2020-12-13 17:21:32 +0800

    Update viewreview.php

--- php/viewreview.php
+++ php/viewreview.php
@@ -1,14 +1,19 @@
 <?php
+session_start();
+session_regenerate_id(); //regenerate new session id
 require('config.php');
-$username = $_SESSION[username];
-$stmt=$con->prepare("SELECT username FROM users WHERE user_id = ?");//Get favorite data from database
+// To check if session is started.
+if(isset($_SESSION["username"]))
+{
+	$username = $_SESSION[username];
+	$stmt=$con->prepare("SELECT username FROM users WHERE user_id = ?");//Get favorite data from database
     $stmt->bind_param("s", $username);
 	$res=$stmt->execute();
     $stmt->store_result();
     $stmt->bind_result($userID); //Bind the data from database
-$userID = $userID;
+	$userID = $userID;
 
-$stmt=$con->prepare("SELECT review.review_id, review.rating, review.content, review.timestamp, product.name FROM review LEFT JOIN product ON review.product_product_id = product.product_id WHERE users_user_id = ?");//Get product data from database
+	$stmt=$con->prepare("SELECT review.review_id, review.rating, review.content, review.timestamp, product.name FROM review LEFT JOIN product ON review.product_product_id = product.product_id WHERE users_user_id = ?");//Get product data from database
 	$stmt->bind_param("i", $userID);
     $res=$stmt->execute();
     $stmt->store_result();
@@ -21,4 +26,15 @@ $stmt=$con->prepare("SELECT review.review_id, review.rating, review.content, rev
             echo "<tr><td>" .$prodName. "</td><td>" .$rating. "/5⭐</td><td>" .$content. "</td><td>" .$timestamp. "</td><td><form action='editcomment.php' method='post'><input type='hidden' value='".$userID."' name='userID'><input type='hidden' value='".$reviewID."' name='reviewID'><input type='submit' value='Edit Review'></form></td>";
     }		
     echo "</table><br>";
+    if(time()-$_SESSION["login_time_stamp"] >600)
+    {
+        session_unset();
+        session_destroy();
+        header("Location:../home.php");
+    }
+}
+else
+{
+    header("Location:../home.php");
+}
 ?>
