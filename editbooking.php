045ee135fb3fb3ae2f1f3a2d4f42c42cc845a177
Clarence <tanweisheng@msn.com>
2020-12-14 13:38:44 +0800

    Update editbooking.php

--- editbooking.php
+++ editbooking.php
@@ -39,8 +39,8 @@ session_regenerate_id(); //session_regenerate_id();
 include 'php/config.php';
 
 if(isset($_SESSION["username"])){
-$startregex= "/^(0[1-9]|[1-2][0-9]|3[0-1])+-(0[1-9]|1[0-2])+-([1-2][0-9]{3})$/";
-$endregex= "/^(0[1-9]|[1-2][0-9]|3[0-1])+-(0[1-9]|1[0-2])+-([1-2][0-9]{3})$/"; 
+$startregex= "/^(0[1-9]|[1-2][0-9]|3[0-1])+-(0[1-9]|1[0-2])+-([1-2][0][2][0-1])$/";
+$endregex= "/^(0[1-9]|[1-2][0-9]|3[0-1])+-(0[1-9]|1[0-2])+-([1-2][0][2][0-1])$/"; 
 
 
 $bookingID=$_POST['booking_id'];
@@ -68,9 +68,9 @@ $result=$pQuery->get_result();
 <form action="php/doeditbooking.php" method="post">
 <h1 align='center'>Edit your booking</h1>
 <table align='center'>
-<tr><td>Please enter the start date in yyyy-mm-dd format</td></tr>
+<tr><td>Please enter the start date in dd-mm-yyyy format</td></tr>
 <tr><td>Start Time: </td><td><input type="text" name="start_time" value="<?php echo $row['start_time']?>"></td></tr>
-<tr><td>Please enter the end date in yyyy-mm-dd format</td></tr>
+<tr><td>Please enter the end date in dd-mm-yyyy format</td></tr>
 <tr><td>End Time: </td><td><input type="text" name="end_time" value="<?php echo $row['end_time']?>"></td></tr>
 <tr><td></td><td>
 <input type='hidden' name='product_id' value=<?php echo $product?>>
